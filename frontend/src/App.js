19f02466075af7c78be86769768ffe646ab96f0b
ChangOliver <b06902119@ntu.edu.tw>
2020-12-15 16:59:23 +0800

    switch page (inprogress)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 import NormalLoginForm from './components/NormalLoginForm'
 import {PieChartOutlined, HeartOutlined, CopyrightOutlined, GithubOutlined} from '@ant-design/icons'
-import React from 'react'
+import React, { useState } from 'react'
 import background from './imgs/background.png'
+import wheel from './imgs/wheel.jpeg'
 
 function App() {
+
+	const [login, setLogin] = useState(false)
+
 	return (
 		<React.Fragment>
-			<img className="background" src={background} alt="Background"/>  
-			<div>
-				<NormalLoginForm></NormalLoginForm>
-			</div> 
-			<div>
-				
-			</div>
+			<img className="background" src={background} alt="Background"/> 
+			{login ? (		 
+				<div>
+					<NormalLoginForm></NormalLoginForm>
+				</div>
+			):(
+				<div>
+					
+				</div>
+			)}
 			<h3 className="footer"> 
 				<PieChartOutlined/> <br/> <CopyrightOutlined /> 2020 by WTF <br/> Proudly created with <HeartOutlined />
 			</h3>
