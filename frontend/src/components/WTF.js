7c2fb9a07d5935d9c22742217106fbe7eade8a19
ChangOliver <b06902119@ntu.edu.tw>
2020-12-15 17:40:23 +0800

    placeholder img

---
+++ frontend/src/components/WTF.js
@@ -0,0 +1,15 @@
+import './NormalLoginForm.css'
+import { } from 'antd';
+import { } from '@ant-design/icons';
+import React, { useState } from 'react'
+import axios from 'axios'
+import wheel from '../imgs/wheel.png'
+
+
+function WTF() {
+	return(
+		<img className="wheel" src={wheel} alt="Wheel"/>
+	);
+};
+
+export default WTF;
