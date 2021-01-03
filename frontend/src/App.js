6be8d7154a4556196da34dfcd3fb050d324bbcfd
ChangOliver <b06902119@ntu.edu.tw>
2021-01-04 03:06:34 +0800

    add github link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Login from './container/Login'
 import WTF from './container/WTF'
 import Development from './container/Development'
-import { PieChartOutlined, HeartFilled, CopyrightOutlined, GithubOutlined } from '@ant-design/icons'
+import { PieChartOutlined, HeartFilled, CopyrightOutlined, GithubOutlined, StarFilled } from '@ant-design/icons'
 import Background from './components/Background'
 import React from 'react'
 import { BrowserRouter, Route } from "react-router-dom"
 import { Button } from 'antd'
+import ChangOliver from './imgs/ChangOliver.png'
+import lin9999 from './imgs/lin9999.png'
+import vincent861223 from './imgs/vincent861223.png'
 
 function App() {
     return (
@@ -21,9 +24,24 @@ function App() {
 				<PieChartOutlined/> <br/> <CopyrightOutlined /> 2020 by WTF <br/> Proudly created with <HeartFilled />
 			</h3>
 			<h3 className="gitIcon">
-				Give us a star: &nbsp; 
-				<Button type="text" href="https://github.com/ChangOliver/Wheel-of-Restaurants" > 
-					 <GithubOutlined/>
+				Give us a <StarFilled />: &nbsp; 
+				<Button 
+					icon = {<GithubOutlined/>}
+					type="text"  
+					shape="circle" 
+					size="large"
+					href="https://github.com/ChangOliver/Wheel-of-Restaurants"
+					target = "_blank" />
+				<br/>
+				WTF Team: &nbsp;
+				<Button type="text" shape="circle" href="https://github.com/ChangOliver/" target="_blank">
+					<img src = {ChangOliver} alt = "ChangOliver" style={{"border-radius":"50%"}}/> 
+				</Button>
+				<Button type="text" shape="circle" href="https://github.com/lin9999" target="_blank">
+					<img src = {lin9999} alt = "lin9999" style={{"border-radius":"50%"}}/> 
+				</Button>
+				<Button type="text" shape="circle" href="https://github.com/vincent861223" target="_blank">
+					<img src = {vincent861223} alt = "vincent861223" style={{"border-radius":"50%"}}/> 
 				</Button>
 			</h3>
 		</React.Fragment>
