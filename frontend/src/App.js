d431c11b9acda1c64c1b44cca68c779c0a9c98b2
林恩廷 <lin.enting@icloud.com>
2021-01-20 21:27:35 +0900

    deploy frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,9 +16,9 @@ function App() {
         <React.StrictMode>
 			<Background/>
             <BrowserRouter>
-				<Route exact path="/" component={Login} />
-				<Route exact path="/WTF" component={WTF} /> 
-				<Route exact path="/Development/Development" component={Development}></Route>
+				<Route path="/" component={Login} />
+				<Route path="/WTF" component={WTF} /> 
+				<Route path="/Development/Development" component={Development}></Route>
             </BrowserRouter>
 			<h3 className="footer"> 
 				<PieChartOutlined/> <br/> <CopyrightOutlined /> 2020 by WTF <br/> Proudly created with <HeartFilled />
