6788c6535a03df53f8070762bf2b6dbb07aadf2d
林恩廷 <lin.enting@icloud.com>
2021-06-11 01:58:54 +0900

    db

--- backend/models/account.js
+++ backend/models/account.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema
 const AccountSchema = Schema({
 		userName: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
-		blacklist: [{ type: String }],
 		favorite: [{ type: String }],
-		identity: { type: String, required: true}
+		blacklist: [{ type: String }],
+		recentVist: [{ type: String }],
+		identity: { type: String, required: true} 
 	}, {
 		_id: true,
 		collection: 'Account',
